6444ae3d01e79030b3e4a73355660ccc7059883a
Noah Litvin <noah.litvin@gmail.com>
2023-06-05 11:15:35 -0400

    pretty fix

--- staking/cypress/cypress/e2e/Pool - Not Connected.e2e.js
+++ staking/cypress/cypress/e2e/Pool - Not Connected.e2e.js
@@ -4,8 +4,7 @@ it('shows homepage to not connected wallet', () => {
   });
   cy.visit('http://localhost:3000/pools/1');
 
-  cy.get('[data-testid="pool collateral types"]')
-    .should('contain', 'Pool Collateralization');
+  cy.get('[data-testid="pool collateral types"]').should('contain', 'Pool Collateralization');
   cy.get('[data-testid="pool collateral"][data-collateral="SNX"]').should('exist');
 
   cy.get('[data-testid="pool markets"]').should('contain', 'Markets');
