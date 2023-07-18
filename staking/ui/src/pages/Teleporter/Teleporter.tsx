f117db2bf76d81dea7a54d131b1d99551c3d19be
fritzschoff <fritzschoff@icloud.com>
2023-07-18 17:28:52 +0200

    Revert "Add link (#48)"
    This reverts commit f92bea43b4384fe429184d8b3f573d9fc63415fb.

--- staking/ui/src/pages/Teleporter/Teleporter.tsx
+++ staking/ui/src/pages/Teleporter/Teleporter.tsx
@@ -9,7 +9,6 @@ import {
   MenuItem,
   MenuList,
   Text,
-  Link,
 } from '@chakra-ui/react';
 import { Balance } from '@snx-v3/Balance';
 import { NumberInput } from '@snx-v3/NumberInput';
@@ -69,15 +68,7 @@ export const TeleporterUi: FC<{
       </Flex>
       <Text mt={2} color="gray.500">
         Teleport your assets between layers using the teleporter. This transaction can take up to 30
-        minutes.{' '}
-        <Link
-          color="cyan.500"
-          target="_blank"
-          href="https://blog.synthetix.io/synthetix-launches-teleporters-with-chainlinks-ccip"
-        >
-          Read more
-        </Link>{' '}
-        about teleporter and CCIP.
+        minutes. Read more about teleporter and CCIP.
       </Text>
       <Divider mt={4} mb={4} />
       <BorderBox flexDirection="column" p="4">
