a0dc1c236f83f1528d616d8c27814205e44ae034
fritzschoff <fritzschoff@icloud.com>
2023-07-26 10:58:22 +0100

    added network list

--- governance/components/Header.tsx
+++ governance/components/Header.tsx
@@ -1,8 +1,26 @@
-import { Button, Flex, Heading, Image, Text } from '@chakra-ui/react';
-import { disconnect, onboard, useIsConnected, useWallet } from '@snx-v3/useBlockchain';
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react';
+import {
+  Network,
+  disconnect,
+  onboard,
+  useIsConnected,
+  useNetwork,
+  useWallet,
+} from '@snx-v3/useBlockchain';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { shortAddress } from '../utils/addresses';
-import { WalletIcon } from '@snx-v3/icons';
+import { EthereumIcon, FailedIcon, OptimismIcon, WalletIcon } from '@snx-v3/icons';
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
 
 const routes = [
   {
@@ -13,11 +31,30 @@ const routes = [
   { path: 'profile', label: 'Profile' },
 ];
 
+const activeIcon = (currentNetwork: Network) => {
+  switch (currentNetwork.id) {
+    case 1:
+      return { icon: <EthereumIcon />, name: 'Ethereum' };
+    case 5:
+      return { icon: <EthereumIcon />, name: 'Goerli Testnet' };
+    case 420:
+      return { icon: <OptimismIcon />, name: 'Optimistic Goerli' };
+
+    default:
+      return { icon: <FailedIcon width="24px" height="24px" />, name: 'Unsupported Network' };
+  }
+};
+
 export default function Header() {
   const navigate = useNavigate();
   const location = useLocation();
   const isWalletConnected = useIsConnected();
   const wallet = useWallet();
+  const currentNetwork = useNetwork();
+  const { name, icon } = activeIcon(currentNetwork);
+  const switchNetwork = async (id: number) => {
+    return onboard?.setChain({ chainId: `0x${id.toString(16)}` });
+  };
   return (
     <Flex as="header" bg="navy.700" h="65px" alignItems="center" px="40px">
       <Image src="/snx-header.svg" pr="10px" />
@@ -33,12 +70,54 @@ export default function Header() {
           fontWeight="700"
           fontSize="14px"
           lineHeight="20px"
-          mr={index === routes.length - 2 ? 'auto' : '32px'}
+          mr={index === routes.length - 2 ? 'auto' : index === routes.length - 1 ? '8px' : '32px'}
         >
           {route.label}
         </Text>
       ))}
-
+      {isWalletConnected && (
+        <Menu>
+          {({ isOpen }) => (
+            <>
+              <MenuButton
+                as={Button}
+                ml={2}
+                variant="outline"
+                colorScheme="gray"
+                sx={{ '> span': { display: 'flex', alignItems: 'center' } }}
+              >
+                {icon}
+                <>
+                  <Text variant="nav" fontSize="sm" fontWeight={700} ml={1.5} mr={2}>
+                    {name}
+                  </Text>
+                  {isOpen ? <ChevronUpIcon color="cyan" /> : <ChevronDownIcon color="cyan.500" />}
+                </>
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={() => switchNetwork(1)}>
+                  <EthereumIcon />
+                  <Text variant="nav" ml={2}>
+                    Ethereum Mainnet
+                  </Text>
+                </MenuItem>
+                <MenuItem onClick={() => switchNetwork(10)}>
+                  <OptimismIcon />
+                  <Text variant="nav" ml={2}>
+                    Optimism
+                  </Text>
+                </MenuItem>
+                <MenuItem onClick={() => switchNetwork(420)}>
+                  <OptimismIcon />
+                  <Text variant="nav" ml={2}>
+                    Optimism Goerli
+                  </Text>
+                </MenuItem>
+              </MenuList>
+            </>
+          )}
+        </Menu>
+      )}
       {isWalletConnected ? (
         <Button
           leftIcon={<WalletIcon />}
@@ -46,6 +125,7 @@ export default function Header() {
           variant="outline"
           borderColor="gray.900"
           color="whiteAlpha.800"
+          ml="8px"
         >
           {shortAddress(wallet?.address || '')}
         </Button>
