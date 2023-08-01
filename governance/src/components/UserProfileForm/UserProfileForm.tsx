61a86cb1be96c7e544f2cc824ad776feee0d43f6
butternutsquash <butternutsquash1900@gmail.com>
2023-08-01 15:09:58 +0100

    added form

---
+++ governance/src/components/UserProfileForm/UserProfileForm.tsx
@@ -0,0 +1,95 @@
+import { CopyIcon } from '@chakra-ui/icons';
+import { Button, Flex, Image, Input, Text, Textarea } from '@chakra-ui/react';
+import { prettyString } from '@snx-v3/format';
+import { useForm } from 'react-hook-form';
+
+interface User {
+  address: string;
+  username: string;
+  discord: string;
+  twitter: string;
+  about: string;
+  github: string;
+  pfpUrl: string;
+  delegationPitch: string;
+}
+
+export function UserProfileForm({ user }: { user: Partial<User> }) {
+  const { register } = useForm({
+    defaultValues: {
+      address: user.about,
+      username: user.username,
+      discord: user.discord,
+      twitter: user.twitter,
+      about: user.about,
+      github: user.github,
+      pffUrl: user.pfpUrl,
+      delegationPitch: user.delegationPitch,
+    },
+  });
+  return (
+    <Flex flexDir="column" gap="4">
+      <Flex w="100%">
+        <Flex flexDir="column" w="56px" mr="4" justifyContent="center">
+          <Image src={user.pfpUrl} w="56px" h="56px" mb="2" />
+          <Button colorScheme="gray" variant="outline">
+            Edit
+          </Button>
+        </Flex>
+        <Flex flexDir="column" w="100%">
+          <Text color="gray.500">Username</Text>
+          <Input {...register('username')} mb="1" placeholder="John Doe" />
+          <Text color="gray.500">About</Text>
+          <Input {...register('about')} placeholder="OG DeFi Member" />
+        </Flex>
+      </Flex>
+      <div>
+        <Text color="gray.500">Discord</Text>
+        <Input {...register('discord')} placeholder="John Doe" />
+      </div>
+      <div>
+        <Text color="gray.500">Twitter</Text>
+        <Input {...register('twitter')} placeholder="John_Doe" />
+      </div>
+      <div>
+        <Text color="gray.500">Github</Text>
+        <Input {...register('github')} placeholder="John Doe" />
+      </div>
+      <div>
+        <Text color="gray.500">Wallet Address</Text>
+        <Input disabled={true} placeholder={user.address} />
+      </div>
+      <Flex flexDirection="column" alignItems="flex-start" mb="6">
+        {/* @TODO what gray is that? */}
+        <Text fontSize="14px" fontWeight="400" color="#828295">
+          Wallet Address
+        </Text>
+        <Button
+          size="xs"
+          display="flex"
+          variant="unstyled"
+          onClick={() => {
+            try {
+              navigator.clipboard.writeText(user.address!);
+            } catch (error) {
+              console.error(error);
+            }
+          }}
+        >
+          <Text mr="1" fontSize="12px" fontWeight="700">
+            {prettyString(user.address || '')}
+          </Text>
+          <CopyIcon w="12px" h="12px" />
+        </Button>
+      </Flex>
+      <div>
+        <Text color="gray.500">Governance Pitch</Text>
+        <Textarea placeholder={'I will pump SNX'} />
+      </div>
+
+      <Button colorScheme="gray" variant="outline">
+        Save Profile
+      </Button>
+    </Flex>
+  );
+}
