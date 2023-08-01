fff7aa73f859c87276395b28828bce3670f673a7
max <39312833+fritzschoff@users.noreply.github.com>
2023-08-01 11:48:17 +0200

    fix merge conflict (#60)
    Co-authored-by: butternutsquash <butternutsquash1900@gmail.com>

--- governance/src/pages/Councils.tsx
+++ governance/src/pages/Councils.tsx
@@ -1,10 +1,12 @@
 import { PassedElectionAccordion } from '../components/PassedElectionAccordion';
+import { UserProfileCard } from '../components/UserProfileCard';
 import councils from '../utils/councils';
 
 export default function Councils() {
   return (
     <>
       <PassedElectionAccordion activeCouncil={councils[0]} />
+      <UserProfileCard walletAddress="0x" />
     </>
   );
 }
