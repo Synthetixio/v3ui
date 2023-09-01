786fe1ffa6aa68b2d62e1774fde8333e11c02589
jmzwar <james@jmzwar.com>
2023-09-01 18:01:14 +0200

    add qc

--- staking/ui/src/pages/Pool/PoolHeader.cy.tsx
+++ staking/ui/src/pages/Pool/PoolHeader.cy.tsx
@@ -1,9 +1,16 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { PoolHeader } from './PoolHeader';
+
+const client = new QueryClient();
 
 describe('Pool page / Header', () => {
   it('should render Pool Header', () => {
     cy.viewport(800, 500);
-    cy.mount(<PoolHeader />);
+    cy.mount(
+      <QueryClientProvider client={client}>
+        <PoolHeader />
+      </QueryClientProvider>
+    );
     cy.get('#app').should('include.text', 'Unknown Pool');
   });
 });
