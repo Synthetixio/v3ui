b97157ff355257a5bff2ed30433b0aa4592ad662
joey <joey@cc.snxdao.io>
2023-09-06 14:43:14 +1000

    Use the new state machine in RepayModal - Again heavily inspired from the DepositModal

--- staking/ui/src/App.tsx
+++ staking/ui/src/App.tsx
@@ -13,7 +13,7 @@ import { useAccountUrlSync } from '@snx-v3/useAccounts';
 import { Router } from './Router';
 import './i18n';
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchInterval: DEFAULT_QUERY_REFRESH_INTERVAL,
