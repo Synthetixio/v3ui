f41996594536c4819ea46854394ef5c885922ed0
joey <joey@cc.snxdao.io>
2023-09-06 14:41:13 +1000

    useRepay need to deposit first - Only deposit what's needed - Also removed unused event handlers

--- staking/lib/useRepay/useRepay.tsx
+++ staking/lib/useRepay/useRepay.tsx
@@ -8,27 +8,26 @@ import Wei from '@synthetixio/wei';
 import { BigNumber } from 'ethers';
 import { getGasPrice } from '@snx-v3/useGasPrice';
 import { useGasSpeed } from '@snx-v3/useGasSpeed';
+import { useUSDProxy } from '@snx-v3/useUSDProxy';
 
-export const useRepay = (
-  {
-    accountId,
-    poolId,
-    collateralTypeAddress,
-    debtChange,
-  }: {
-    accountId?: string;
-    poolId?: string;
-    collateralTypeAddress?: string;
-    debtChange: Wei;
-  },
-  eventHandlers?: {
-    onSuccess?: () => void;
-    onMutate?: () => void;
-    onError?: (e: Error) => void;
-  }
-) => {
+export const useRepay = ({
+  accountId,
+  poolId,
+  collateralTypeAddress,
+  debtChange,
+  balance,
+  availableUSDCollateral,
+}: {
+  accountId?: string;
+  poolId?: string;
+  collateralTypeAddress?: string;
+  balance?: Wei;
+  availableUSDCollateral?: Wei;
+  debtChange: Wei;
+}) => {
   const [txnState, dispatch] = useReducer(reducer, initialState);
   const { data: CoreProxy } = useCoreProxy();
+  const { data: UsdProxy } = useUSDProxy();
 
   const signer = useSigner();
   const { gasSpeed } = useGasSpeed();
@@ -37,26 +36,37 @@ export const useRepay = (
   const mutation = useMutation({
     mutationFn: async () => {
       if (!signer) return;
-      if (!(CoreProxy && poolId && accountId && collateralTypeAddress)) return;
+      if (!(CoreProxy && poolId && accountId && collateralTypeAddress && UsdProxy)) return;
+      if (!balance) return;
+      if (!availableUSDCollateral) return;
       if (debtChange.eq(0)) return;
+      const debtChangeAbs = debtChange.abs();
+      const amountToDeposit = debtChangeAbs.sub(availableUSDCollateral);
 
       try {
         dispatch({ type: 'prompting' });
+        // Only deposit if user doesn't have enough sUSD collateral
+        const deposit = amountToDeposit.lte(0)
+          ? undefined
+          : CoreProxy.interface.encodeFunctionData('deposit', [
+              BigNumber.from(accountId),
+              UsdProxy.address,
+              amountToDeposit.toBN(), // only deposit what's needed
+            ]);
+
+        const burn = CoreProxy.interface.encodeFunctionData('burnUsd', [
+          BigNumber.from(accountId),
+          BigNumber.from(poolId),
+          collateralTypeAddress,
+          debtChangeAbs.toBN(),
+        ]);
+        const calls = [deposit, burn].filter(Boolean) as string[];
 
         const gasPricesPromised = getGasPrice({ provider });
-        const gasLimitPromised = CoreProxy.estimateGas.burnUsd(
-          BigNumber.from(accountId),
-          BigNumber.from(poolId),
-          collateralTypeAddress,
-          debtChange.abs().toBN()
-        );
-        const populatedTxnPromised = CoreProxy.populateTransaction.burnUsd(
-          BigNumber.from(accountId),
-          BigNumber.from(poolId),
-          collateralTypeAddress,
-          debtChange.abs().toBN(),
-          { gasLimit: gasLimitPromised }
-        );
+        const gasLimitPromised = CoreProxy.estimateGas.multicall(calls);
+        const populatedTxnPromised = CoreProxy.populateTransaction.multicall(calls, {
+          gasLimit: gasLimitPromised,
+        });
         const [gasPrices, gasLimit, populatedTxn] = await Promise.all([
           gasPricesPromised,
           gasLimitPromised,
@@ -79,7 +89,6 @@ export const useRepay = (
         throw error;
       }
     },
-    ...eventHandlers,
   });
   return {
     mutation,
