964105a94472718ac151201fee938af7e793ef70
Noisekit <nikita@cc.snxdao.io>
2023-10-17 22:11:51 +0800

    v3.synthetix.eth (#91)
    * Synthetix v3 website
    
    * Tagged auto-deploy for website
    
    * Move index.html to the root
    
    * Add favicon and redirects
    
    * yarn dedupe
    
    * Pretty

---
+++ website/ui/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        inconsolata: ['Inconsolata', 'monospace'],
+      },
+      fontSize: {
+        bodyText: '15px',
+        header: '23px',
+      },
+      lineHeight: {
+        body: '26px',
+      },
+      letterSpacing: {
+        body: '0.01em',
+        header: '0.16em',
+        tab: '0.12em',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+    },
+  },
+  plugins: [],
+};
