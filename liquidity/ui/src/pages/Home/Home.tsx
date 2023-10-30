f3a267e6d15c2479a08f67d8a5cf0743b3daf6c5
jmzwar <james@jmzwar.com>
2023-10-30 16:00:49 +0100

    update home

--- liquidity/ui/src/pages/Home/Home.tsx
+++ liquidity/ui/src/pages/Home/Home.tsx
@@ -1,24 +1,24 @@
+import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Flex } from '@chakra-ui/react';
-import React from 'react';
-import { CollateralType, useCollateralTypes } from '@snx-v3/useCollateralTypes';
-import { PoolsType, usePools } from '@snx-v3/usePools';
+import { useCollateralTypes } from '@snx-v3/useCollateralTypes';
+import { usePools } from '@snx-v3/usePools';
 import { useParams } from '@snx-v3/useParams';
-import { LiquidityPositionsById, useLiquidityPositions } from '@snx-v3/useLiquidityPositions';
+import { useLiquidityPositions } from '@snx-v3/useLiquidityPositions';
 import { Welcome } from '../../components/Shared/Welcome';
 import { PoolsList, Stats, AvailableCollateral } from '@snx-v3/Pools';
 
-export function HomeUi({
-  collateralTypes,
-  pools,
-  liquidityPositionsById,
-  isLoading,
-}: {
-  collateralTypes?: CollateralType[];
-  pools?: PoolsType;
-  liquidityPositionsById?: LiquidityPositionsById;
-  isLoading: boolean;
-}) {
+export function Home() {
+  const { accountId } = useParams();
+
+  const { data: collateralTypes = [], isLoading: collateralTypesLoading } = useCollateralTypes();
+  const { data: pools, isLoading: isPoolsLoading } = usePools();
+
+  const { data: liquidityPositionsById, isLoading: liquidityPositionLoading } =
+    useLiquidityPositions({ accountId });
+
+  const isLoading = collateralTypesLoading || isPoolsLoading || liquidityPositionLoading;
+
   const { totalCollateral, totalDebt } =
     Object.values(liquidityPositionsById || []).reduce(
       (acc, val) => {
@@ -30,49 +30,22 @@ export function HomeUi({
     ) || {};
 
   return (
-    <Flex height="100%" flexDirection="column">
-      <Welcome />
-      <Stats totalDebt={totalDebt} totalCollateral={totalCollateral} isLoading={isLoading} />
-      <PoolsList
-        pools={pools}
-        isLoading={isLoading}
-        collateralTypes={collateralTypes}
-        liquidityPositionsById={liquidityPositionsById}
-      />
-      <AvailableCollateral />
-    </Flex>
-  );
-}
-
-export function Home() {
-  const { accountId } = useParams();
-
-  const { data: collateralTypes = [], isLoading: collateralTypesLoading } = useCollateralTypes();
-  const { data: pools, isLoading: isPoolsLoading } = usePools();
-
-  const {
-    data: liquidityPositionsById,
-    isLoading: liquidityPositionLoading,
-    isInitialLoading: liquidityInitialLoading,
-  } = useLiquidityPositions({ accountId });
-
-  const isLoading =
-    collateralTypesLoading ||
-    isPoolsLoading ||
-    (liquidityPositionLoading && liquidityInitialLoading);
-
-  return (
     <>
       <Helmet>
         <title>Synthetix V3</title>
         <meta name="description" content="Synthetix V3" />
       </Helmet>
-      <HomeUi
-        isLoading={isLoading}
-        liquidityPositionsById={liquidityPositionsById}
-        collateralTypes={collateralTypes}
-        pools={pools}
-      />
+      <Flex height="100%" flexDirection="column">
+        <Welcome />
+        <Stats totalDebt={totalDebt} totalCollateral={totalCollateral} isLoading={isLoading} />
+        <PoolsList
+          pools={pools}
+          isLoading={isLoading}
+          collateralTypes={collateralTypes}
+          liquidityPositionsById={liquidityPositionsById}
+        />
+        <AvailableCollateral />
+      </Flex>
     </>
   );
 }
