aedee53c3fedd85620934f02bde3dfa3cae58bc6
joey <joey@cc.snxdao.io>
2023-11-15 11:30:23 +1100

    Update useAccountCollateralUnlockDate to use TrustedMulticallForwarder

--- liquidity/lib/useAccountCollateralUnlockDate/useAccountCollateralUnlockDate.ts
+++ liquidity/lib/useAccountCollateralUnlockDate/useAccountCollateralUnlockDate.ts
@@ -2,25 +2,36 @@ import { ethers } from 'ethers';
 import { useQuery } from '@tanstack/react-query';
 import { useCoreProxy } from '@snx-v3/useCoreProxy';
 import { useNetwork } from '@snx-v3/useBlockchain';
+import { useTrustedMulticallForwarder } from '@snx-v3/useTrustedMulticallForwarder';
 
 export function useAccountCollateralUnlockDate({ accountId }: { accountId?: string }) {
   const { data: CoreProxy } = useCoreProxy();
+  const { data: TrustedMulticallForwarder } = useTrustedMulticallForwarder();
   const network = useNetwork();
 
   return useQuery({
     queryKey: [`${network.id}-${network.preset}`, 'AccountCollateralUnlockDate', { accountId }],
     enabled: Boolean(CoreProxy && accountId),
     queryFn: async function () {
-      if (!CoreProxy || !accountId) throw 'OMG';
-
-      const [getAccountLastInteraction, getConfigUintAccountTimeoutWithdraw] =
-        await CoreProxy.callStatic.multicall([
-          CoreProxy.interface.encodeFunctionData('getAccountLastInteraction', [accountId]),
-          CoreProxy.interface.encodeFunctionData('getConfigUint', [
+      if (!CoreProxy || !accountId || !TrustedMulticallForwarder) {
+        throw Error('useAccountCollateralUnlockDate should not be enabled');
+      }
+      const {
+        returnData: [getAccountLastInteraction, getConfigUintAccountTimeoutWithdraw],
+      } = await TrustedMulticallForwarder.callStatic.aggregate([
+        {
+          target: CoreProxy.address,
+          callData: CoreProxy.interface.encodeFunctionData('getAccountLastInteraction', [
+            accountId,
+          ]),
+        },
+        {
+          target: CoreProxy.address,
+          callData: CoreProxy.interface.encodeFunctionData('getConfigUint', [
             ethers.utils.formatBytes32String('accountTimeoutWithdraw'),
           ]),
-        ]);
-
+        },
+      ]);
       const [lastInteraction] = CoreProxy.interface.decodeFunctionResult(
         'getAccountLastInteraction',
         getAccountLastInteraction
