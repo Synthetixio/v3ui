5b8320c71349feb6dcbf04c0e04c78df3399128d
fritzschoff <fritzschoff@icloud.com>
2024-01-04 13:24:09 +0000

    update address

--- governance/ui/src/utils/contracts.ts
+++ governance/ui/src/utils/contracts.ts
@@ -3,35 +3,21 @@ import { electionModuleABI } from './abi';
 import { CouncilSlugs } from './councils';
 
 export const SpartanCouncilContract = new Contract(
-  process.env.DEV
-    ? '0x62F424908BEaF103d0Dd1e0b230356A3785e409d'
-    : process.env.TESTNET
-      ? '0x62F424908BEaF103d0Dd1e0b230356A3785e409d'
-      : '0x62F424908BEaF103d0Dd1e0b230356A3785e409d',
+  '0x62F424908BEaF103d0Dd1e0b230356A3785e409d',
   electionModuleABI
 );
 export const AmbassadorCouncilContract = new Contract(
-  process.env.DEV
-    ? '0x62F424908BEaF103d0Dd1e0b230356A3785e409d'
-    : process.env.TESTNET
-      ? '0x62F424908BEaF103d0Dd1e0b230356A3785e409d'
-      : '0x62F424908BEaF103d0Dd1e0b230356A3785e409d',
+  '0x62F424908BEaF103d0Dd1e0b230356A3785e409d',
+
   electionModuleABI
 );
 export const GrantsCouncilContract = new Contract(
-  process.env.DEV
-    ? '0x62F424908BEaF103d0Dd1e0b230356A3785e409d'
-    : process.env.TESTNET
-      ? '0x62F424908BEaF103d0Dd1e0b230356A3785e409d'
-      : '0x62F424908BEaF103d0Dd1e0b230356A3785e409d',
+  '0x62F424908BEaF103d0Dd1e0b230356A3785e409d',
+
   electionModuleABI
 );
 export const TreasuryCouncilContract = new Contract(
-  process.env.DEV
-    ? '0x62F424908BEaF103d0Dd1e0b230356A3785e409d'
-    : process.env.TESTNET
-      ? '0x62F424908BEaF103d0Dd1e0b230356A3785e409d'
-      : '0x62F424908BEaF103d0Dd1e0b230356A3785e409d',
+  '0x62F424908BEaF103d0Dd1e0b230356A3785e409d',
   electionModuleABI
 );
 
