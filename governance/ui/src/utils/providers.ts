4bd48cc9e86abc2d675951b4b9f70286b053f961
fritzschoff <fritzschoff@icloud.com>
2024-01-26 10:25:02 +0000

    updated subgraph

--- governance/ui/src/utils/providers.ts
+++ governance/ui/src/utils/providers.ts
@@ -1,25 +1,28 @@
 import { providers } from 'ethers';
 
+// Sepolia for testnet
 export const motherShipProvider = new providers.JsonRpcProvider(
   process.env.DEV
-    ? 'http://127.0.0.1:19000/'
+    ? 'https://ethereum-sepolia.publicnode.com'
     : process.env.TESTNET
       ? `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`
       : 'https://mainnet.optimism.io'
 );
 
+// Mumbai for testnet
 export const satellite1Provider = new providers.JsonRpcProvider(
   process.env.DEV
-    ? 'http://127.0.0.1:19001/'
+    ? 'https://endpoints.omniatech.io/v1/matic/mumbai/public'
     : process.env.TESTNET
-      ? `https://optimistic-goerli.infura.io/v3/${process.env.INFURA_KEY}`
+      ? `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY}`
       : 'https://mainnet.optimism.io'
 );
 
+// OP Goerli for testnet
 export const satellite2Provider = new providers.JsonRpcProvider(
   process.env.DEV
-    ? 'http://127.0.0.1:19002/'
+    ? 'https://endpoints.omniatech.io/v1/op/goerli/public'
     : process.env.TESTNET
-      ? `https://avalanche-fuji.infura.io/v3/${process.env.INFURA_KEY}`
+      ? `https://optimistic-goerli.infura.io/v3/${process.env.INFURA_KEY}`
       : 'https://mainnet.optimism.io'
 );
