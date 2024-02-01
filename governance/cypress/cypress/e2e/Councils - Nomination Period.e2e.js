9fca45367476858f3df4ab99226e88b279042bae
fritzschoff <fritzschoff@icloud.com>
2024-02-01 14:34:50 +0000

    added council e2e

---
+++ governance/cypress/cypress/e2e/Councils - Nomination Period.e2e.js
@@ -0,0 +1,14 @@
+it('shows council card and header in nomination period', () => {
+  cy.on('window:before:load', (win) => {
+    win.sessionStorage.TERMS_CONDITIONS_ACCEPTED = 'true';
+    win.localStorage.setItem('connectedWallets', '["MetaMask"]');
+    win.localStorage.setItem('defaultWallet', '"MetaMask"');
+  });
+  cy.connectWallet().then((signer) => {
+    cy.task('setEthBalance', { address: signer.address, balance: 100 });
+    cy.task('setToNominationPeriod');
+  });
+  cy.viewport(1100, 900);
+  cy.visit('/#/councils/spartan');
+  cy.get('[data-testid="period-countdown"]').contains('Voting starts');
+});
