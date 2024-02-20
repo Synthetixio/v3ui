4f398da1bdfa6d96a7f082ff9a28f06276a6e8ed
max <39312833+fritzschoff@users.noreply.github.com>
2024-02-20 08:51:51 +0000

    positions-table (#159)

--- liquidity/ui/src/pages/Dashboard/Dashboard.tsx
+++ liquidity/ui/src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
 import { Flex, Heading, Skeleton, Text } from '@chakra-ui/react';
 import { useLiquidityPositions } from '@snx-v3/useLiquidityPositions';
 import Wei from '@synthetixio/wei';
+import { Helmet } from 'react-helmet';
 import { useSearchParams } from 'react-router-dom';
 import { AssetsList } from '../../components';
+import Positions from '../../components/Positions/Positions';
 
 export function Dashboard() {
   const [params] = useSearchParams();
@@ -14,81 +16,89 @@ export function Dashboard() {
     positions && Object.values(positions).reduce((prev, cur) => prev.add(cur.debt), new Wei(0));
 
   return (
-    <Flex flexDir="column">
-      <Heading>Dashboard</Heading>
-      <Flex w="100%" gap="4">
-        <Flex
-          border="1px solid"
-          borderColor="gray.900"
-          rounded="base"
-          flexDir="column"
-          alignItems="center"
-          justifyContent="center"
-          w="297px"
-          height="88px"
-        >
-          <Text fontSize="14px" color="gray.500">
-            Total Assets
-          </Text>
-          <Text fontSize="24px" fontWeight={800}>
-            TODO
-          </Text>
-        </Flex>
-        <Flex
-          border="1px solid"
-          borderColor="gray.900"
-          rounded="base"
-          flexDir="column"
-          alignItems="center"
-          justifyContent="center"
-          w="297px"
-          height="88px"
-        >
-          <Text fontSize="14px" color="gray.500">
-            Total Delegated
-          </Text>
-          <Text fontSize="24px" fontWeight={800}>
-            TODO
-          </Text>
-        </Flex>
-        <Flex
-          border="1px solid"
-          borderColor="gray.900"
-          rounded="base"
-          flexDir="column"
-          alignItems="center"
-          justifyContent="center"
-          w="297px"
-          height="88px"
-        >
-          <Text fontSize="14px" color="gray.500">
-            Total Debt
-          </Text>
-          <Skeleton isLoaded={!isLoading}>
-            <Text fontSize="24px" fontWeight={800}>
-              ${debt && debt.toNumber().toFixed(2)}
+    <>
+      <Helmet>
+        <title>Synthetix V3 - Dashboard</title>
+        <meta name="description" content="Synthetix V3 - Dashboard" />
+      </Helmet>
+
+      <Flex flexDir="column">
+        <Heading>Dashboard</Heading>
+        <Flex w="100%" gap="4">
+          <Flex
+            border="1px solid"
+            borderColor="gray.900"
+            rounded="base"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="center"
+            w="297px"
+            height="88px"
+          >
+            <Text fontSize="14px" color="gray.500">
+              Total Assets
             </Text>
-          </Skeleton>
+            <Text fontSize="24px" fontWeight={800}>
+              TODO
+            </Text>
+          </Flex>
+          <Flex
+            border="1px solid"
+            borderColor="gray.900"
+            rounded="base"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="center"
+            w="297px"
+            height="88px"
+          >
+            <Text fontSize="14px" color="gray.500">
+              Total Delegated
+            </Text>
+            <Text fontSize="24px" fontWeight={800}>
+              TODO
+            </Text>
+          </Flex>
+          <Flex
+            border="1px solid"
+            borderColor="gray.900"
+            rounded="base"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="center"
+            w="297px"
+            height="88px"
+          >
+            <Text fontSize="14px" color="gray.500">
+              Total Debt
+            </Text>
+            <Skeleton isLoaded={!isLoading}>
+              <Text fontSize="24px" fontWeight={800}>
+                ${debt && debt.toNumber().toFixed(2)}
+              </Text>
+            </Skeleton>
+          </Flex>
+          <Flex
+            border="1px solid"
+            borderColor="gray.900"
+            rounded="base"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="center"
+            w="297px"
+            height="88px"
+          >
+            <Text fontSize="14px" color="gray.500">
+              APY
+            </Text>
+            <Text fontSize="24px" fontWeight={800}>
+              TODO
+            </Text>
+          </Flex>
         </Flex>
-        <Flex
-          border="1px solid"
-          borderColor="gray.900"
-          rounded="base"
-          flexDir="column"
-          alignItems="center"
-          justifyContent="center"
-          w="297px"
-          height="88px"
-        >
-          <Text fontSize="14px" color="gray.500">
-            APY
-          </Text>
-          <Text fontSize="24px" fontWeight={800}>
-            TODO
-          </Text>
-        </Flex>
+        <AssetsList />
+        <Positions />
       </Flex>
-      <AssetsList />
-    </Flex>
+    </>
   );
 }
