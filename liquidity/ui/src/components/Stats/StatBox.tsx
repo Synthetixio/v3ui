bfcf59f7d46ba66ef0511e57155c4654e1c1111a
jmzwar <james@jmzwar.com>
2024-02-26 20:21:57 -0800

    stats card

---
+++ liquidity/ui/src/components/Stats/StatBox.tsx
@@ -0,0 +1,54 @@
+import { InfoIcon } from '@chakra-ui/icons';
+import { Flex, Text, Tooltip, Fade } from '@chakra-ui/react';
+import { SynthSkeleton as Skeleton } from '../Shared';
+
+interface StatBoxProps {
+  isLoading: boolean;
+  title: string;
+  label?: string;
+  value?: string;
+}
+
+export const StatBox = ({ isLoading, title, label, value }: StatBoxProps) => {
+  return (
+    <Flex
+      bg="navy.700"
+      border="1px solid"
+      borderColor="gray.900"
+      rounded="base"
+      flexDir="column"
+      alignItems="center"
+      justifyContent="space-between"
+      w="297px"
+      height="88px"
+      px={6}
+      py={4}
+    >
+      <Flex alignItems="center" mb={1}>
+        <Text fontSize="14px" color="gray.500" mr={1}>
+          {title}
+        </Text>
+        {label && (
+          <Tooltip label={label} p={2} mt={1}>
+            <InfoIcon w="10px" h="10px" />
+          </Tooltip>
+        )}
+      </Flex>
+      <Flex w="100%" justifyContent="center" height="36px" alignItems="center">
+        <Skeleton
+          isLoaded={!isLoading}
+          height="24px"
+          maxWidth={isLoading ? '40%' : 'initial'}
+          startColor="gray.700"
+          endColor="navy.800"
+        >
+          <Fade in>
+            <Text fontSize="24px" lineHeight="24px" fontWeight={800}>
+              {value || 'Place'}
+            </Text>
+          </Fade>
+        </Skeleton>
+      </Flex>
+    </Flex>
+  );
+};
