ac7258e89a84dc9e6c140cc4ae27d0e17b6a18f5
fritzschoff <fritzschoff@icloud.com>
2024-03-07 15:56:23 +0000

    refactor(home): if already assigned we dont add the deposited collateral to total assets calc

--- liquidity/ui/src/pages/Home/Home.tsx
+++ liquidity/ui/src/pages/Home/Home.tsx
@@ -43,11 +43,13 @@ export function Home() {
       const assigned = asset.collateral.totalAssigned.mul(asset.price);
       const deposited = asset.collateral.totalDeposited.mul(asset.price);
       const wallet = asset.balance.mul(asset.price);
-      return assigned.add(deposited).add(wallet);
+      // if already assigned to pool, we dont add it
+      return assigned !== deposited ? assigned.add(wallet) : assigned.add(wallet).add(deposited);
     })
     .reduce((prev, cur) => prev.add(cur), new Wei(0))
     .toNumber()
     .toFixed(2);
+
   const isLoading =
     isAccountCollateralsLoading ||
     tokenBalancesIsLoading ||
