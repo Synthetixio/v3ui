7256af47252b00d66768b772f00c7a8d93ec1ca9
Peiman <25097709+Rickk137@users.noreply.github.com>
2024-03-25 14:45:25 +0330

    fix: UI issues (#197)
    * fix: UI issues
    
    * fix: remove unused manuall contracts

--- contracts/src/importSpotMarketProxy.ts
+++ contracts/src/importSpotMarketProxy.ts
@@ -5,8 +5,6 @@ import type { SpotMarketProxy as SpotMarketProxy13370Main } from './13370-main/S
 import type { SpotMarketProxy as SpotMarketProxy42161Arbthetix } from './42161-arbthetix/SpotMarketProxy';
 import type { SpotMarketProxy as SpotMarketProxy421614Arbthetix } from './421614-arbthetix/SpotMarketProxy';
 import type { SpotMarketProxy as SpotMarketProxy8453Andromeda } from './8453-andromeda/SpotMarketProxy';
-import type { SpotMarketProxy as SpotMarketProxy84531Andromeda } from './84531-andromeda/SpotMarketProxy';
-import type { SpotMarketProxy as SpotMarketProxy84531Main } from './84531-main/SpotMarketProxy';
 import type { SpotMarketProxy as SpotMarketProxy84532Andromeda } from './84532-andromeda/SpotMarketProxy';
 
 export type SpotMarketProxyType =
@@ -15,8 +13,6 @@ export type SpotMarketProxyType =
   | SpotMarketProxy42161Arbthetix
   | SpotMarketProxy421614Arbthetix
   | SpotMarketProxy8453Andromeda
-  | SpotMarketProxy84531Andromeda
-  | SpotMarketProxy84531Main
   | SpotMarketProxy84532Andromeda;
 
 export async function importSpotMarketProxy(chainId: number, preset: string = 'main') {
@@ -31,10 +27,6 @@ export async function importSpotMarketProxy(chainId: number, preset: string = 'm
       return import('./421614-arbthetix/SpotMarketProxy');
     case '8453-andromeda':
       return import('./8453-andromeda/SpotMarketProxy');
-    case '84531-andromeda':
-      return import('./84531-andromeda/SpotMarketProxy');
-    case '84531-main':
-      return import('./84531-main/SpotMarketProxy');
     case '84532-andromeda':
       return import('./84532-andromeda/SpotMarketProxy');
     default:
