472ab02339254bf481d628893dd85946c9fab5f9
jmzwar <james@jmzwar.com>
2024-04-05 17:26:02 +1100

    69

--- liquidity/lib/useRewards/useRewards.ts
+++ liquidity/lib/useRewards/useRewards.ts
@@ -58,7 +58,7 @@ export function useRewards(
   distributors?: RewardsInterface,
   poolId?: string,
   collateralAddress?: string,
-  accountId: string = '1'
+  accountId: string = '69'
 ) {
   const { network } = useNetwork();
   const { data: Multicall3 } = useMulticall3();
