bb7624f132fb1b437ceccec9f9cd6611fa575065
fritzschoff <fritzschoff@icloud.com>
2024-04-19 13:20:51 +0100

    feat(account-selector): added account selector dropdown

--- liquidity/ui/src/layouts/Default/Header.tsx
+++ liquidity/ui/src/layouts/Default/Header.tsx
@@ -2,7 +2,6 @@ import { Box, Container, Flex, Link, useDisclosure } from '@chakra-ui/react';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
 import { NetworkController } from './NetworkController';
 import { useEffect } from 'react';
-import { AccountsSelector } from '@snx-v3/AccountsSelector';
 import { Logo, LogoIcon } from '@snx-v3/icons';
 
 export default function Header() {
@@ -35,9 +34,6 @@ export default function Header() {
             </Link>
           </Box>
           <Flex justifyContent="center" alignItems="center">
-            <Box display={{ base: 'none', md: 'inline-block' }} mr={3}>
-              <AccountsSelector />
-            </Box>
             <NetworkController />
           </Flex>
         </Container>
