e2b8877e465dd4c24f71b1b097371706360ef1a4
max <39312833+fritzschoff@users.noreply.github.com>
2024-05-03 14:20:56 +0100

    Om update (#259)
    * feat(): added mutation and hooks
    
    * support erc7412
    
    * fix(NodeStateButton)
    
    * feat(queries): increased staletime
    
    * feat(support-non-erc7412)
    
    * fix(deps)

--- oracle-manager/ui/src/RegisteredNode.tsx
+++ oracle-manager/ui/src/RegisteredNode.tsx
@@ -1,6 +1,6 @@
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import { Flex, Heading, Link, Text } from '@chakra-ui/react';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { Chart } from '../components/Chart';
@@ -18,59 +18,61 @@ let x = 0;
 let y = 0;
 
 export const RegisteredNode: FC = () => {
-  const [, setNodes] = useRecoilState(nodesState);
+  const [nodes, setNodes] = useRecoilState(nodesState);
   const param = useParams();
   const nodeID = param?.nodeId;
   const networkParam = param?.network ? Number(param.network) : undefined;
-  const provider = new providers.JsonRpcProvider(
-    NETWORKS.filter((network) => network.id === networkParam)[0].rpcUrl()
+  const provider = useMemo(
+    () =>
+      new providers.JsonRpcProvider(
+        NETWORKS.filter((network) => network.id === networkParam)[0].rpcUrl()
+      ),
+    [networkParam]
   );
-  const fetchNode = async (id: string) => {
-    if (networkParam) {
-      const contract = await getNodeModuleContract(
-        provider,
-        NETWORKS.find((network) => network.id === networkParam)!
-      );
-      const node = await contract.getNode(id);
-      const nodeParams = decodeBytesByNodeType(node.nodeType, node.parameters);
-      if (node.nodeType === 0) return;
-      setNodes((state) => {
-        const exists = state.find((state) => state.id === id);
-        if (!exists) {
-          x -= 300;
-          y -= 300;
-          return [
-            ...state,
-            {
-              data: { label: nodeInformationByNodeIds(node.nodeType).label },
-              id: id,
-              parameters: nodeParams as any[],
-              parents: node.parents,
-              source: '',
-              target: '',
-              type: nodeInformationByNodeIds(node.nodeType).slug as OracleNodeTypes,
-              position: { x, y },
-              typeId: node.nodeType,
-              isRegistered: true,
-              network: networkParam,
-            },
-          ];
-        }
-        return state;
-      });
-      if (node.parents.length) {
-        node.parents.map((id: string) => fetchNode(id));
-      }
-    }
-  };
 
   useEffect(() => {
-    setNodes([]);
-    if (nodeID) {
+    if (!nodes.find((node) => node.id === nodeID) && nodeID) {
+      const fetchNode = async (id: string) => {
+        if (networkParam) {
+          const contract = await getNodeModuleContract(
+            provider,
+            NETWORKS.find((network) => network.id === networkParam)!
+          );
+          const node = await contract.getNode(id);
+          const nodeParams = decodeBytesByNodeType(node.nodeType, node.parameters);
+          if (node.nodeType === 0) return;
+          setNodes((state) => {
+            const exists = state.find((state) => state.id === id);
+            if (!exists) {
+              x -= 300;
+              y -= 300;
+              return [
+                ...state,
+                {
+                  data: { label: nodeInformationByNodeIds(node.nodeType).label },
+                  id: id,
+                  parameters: nodeParams as any[],
+                  parents: node.parents,
+                  source: '',
+                  target: '',
+                  type: nodeInformationByNodeIds(node.nodeType).slug as OracleNodeTypes,
+                  position: { x, y },
+                  typeId: node.nodeType,
+                  isRegistered: true,
+                  network: networkParam,
+                },
+              ];
+            }
+            return state;
+          });
+          if (node.parents.length) {
+            node.parents.map((id: string) => fetchNode(id));
+          }
+        }
+      };
       fetchNode(nodeID);
     }
-    // eslint-disable-next-line
-  }, [nodeID]);
+  }, [nodeID, networkParam, nodes, provider, setNodes]);
 
   return (
     <Flex p="10" flexDir="column" gap="5">
