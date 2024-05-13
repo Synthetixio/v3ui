7cd3e4dda17e4364b70289827e25c24ebe3ba527
Peiman <25097709+Rickk137@users.noreply.github.com>
2024-05-13 18:56:54 +0330

    fix: ultrasound issues (#264)

--- ultrasound/ui/hooks/SNXUSDBalanceOfBuyBackContract.ts
+++ ultrasound/ui/hooks/SNXUSDBalanceOfBuyBackContract.ts
@@ -1,17 +1,21 @@
+import { useGetNetwork, useProviderForChain } from '@snx-v3/useBlockchain';
 import { useQuery } from '@tanstack/react-query';
-import { BigNumber, Contract, providers } from 'ethers';
+import { BigNumber, Contract } from 'ethers';
 
-const USDC = new Contract(
-  '0x09d51516F38980035153a554c26Df3C6f51a23C3',
-  ['function balanceOf(address _owner) view returns (uint256 balance)'],
-  new providers.JsonRpcProvider('https://base.llamarpc.com')
-);
+export function SNXUSDBalanceOfBuyBackContract(contractAddress: string) {
+  const baseNetwork = useGetNetwork(`0x${Number(8453).toString(16)}`);
+  const baseProvider = useProviderForChain(baseNetwork);
 
-export function SNXUSDBalanceOfBuyBackContract(contract: string) {
   return useQuery({
-    queryKey: ['USDCBalanceOfBuyBackContract'],
+    queryKey: ['USDCBalanceOfBuyBackContract', contractAddress],
     queryFn: async () => {
-      const balance: BigNumber = await USDC.balanceOf(contract);
+      const USDC = new Contract(
+        '0x09d51516F38980035153a554c26Df3C6f51a23C3',
+        ['function balanceOf(address _owner) view returns (uint256 balance)'],
+        baseProvider
+      );
+
+      const balance: BigNumber = await USDC.balanceOf(contractAddress);
       return balance;
     },
     refetchInterval: 100000,
