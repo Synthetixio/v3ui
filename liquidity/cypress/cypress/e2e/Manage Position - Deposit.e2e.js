509a609e60531c0fb4bdfc15bceeb7dddc1a2091
fritzschoff <fritzschoff@icloud.com>
2024-05-15 11:04:04 +0100

    test(deposit): mine block after navigating to manage page

--- liquidity/cypress/cypress/e2e/Manage Position - Deposit.e2e.js
+++ liquidity/cypress/cypress/e2e/Manage Position - Deposit.e2e.js
@@ -26,6 +26,7 @@ it('should deposit additional collateral', () => {
         cy.visit(`/#${path}?manageAction=deposit&accountId=${accountId}`);
         cy.wait(1000);
       });
+      cy.task('mineBlock');
       cy.get('[data-cy="manage-action-deposit"]').click();
 
       cy.get('[data-testid="deposit amount input"]').type(isBase ? '101' : '10');
@@ -67,9 +68,14 @@ it('should deposit additional collateral', () => {
         .and('include.text', 'Complete this action');
 
       cy.get('[data-cy="deposit-modal"]')
-        .should('include.text', 'Approve WETH transfer')
-        .and('include.text', 'Delegate WETH')
-        .and('include.text', 'This will deposit and delegate 10 WETH to Spartan Council Pool.');
+        .should('include.text', isBase ? 'Approve USDC transfer' : 'Approve WETH transfer')
+        .and('include.text', isBase ? 'Delegate USDC' : 'Delegate WETH')
+        .and(
+          'include.text',
+          isBase
+            ? 'This will deposit and delegate 101 USDC to Spartan Council Pool'
+            : 'This will deposit and delegate 10 WETH to Spartan Council Pool.'
+        );
 
       cy.get('[data-cy="deposit-confirm-button"]').should('include.text', 'Start').click();
 
@@ -80,7 +86,10 @@ it('should deposit additional collateral', () => {
 
       cy.get('[data-cy="deposit-modal"]').should('not.exist');
 
-      cy.get('[data-cy="manage stats collateral"]').should('include.text', '20 WETH');
+      cy.get('[data-cy="manage stats collateral"]').should(
+        'include.text',
+        isBase ? '202 USDC' : '20 WETH'
+      );
     });
   });
 });
