83aa718c3420de44fe74e3e2963f34387957de1b
Peiman <peymannaderi3@yahoo.com>
2024-05-22 21:04:02 +0330

    fix: lintting issue

--- liquidity/ui/src/components/AccountBanner/AccountBanner.tsx
+++ liquidity/ui/src/components/AccountBanner/AccountBanner.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Heading, Text } from '@chakra-ui/react';
+import { Button, Flex, Heading } from '@chakra-ui/react';
 import { useAccounts, useCreateAccount } from '@snx-v3/useAccounts';
 import { FC } from 'react';
 
