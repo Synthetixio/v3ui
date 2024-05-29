4a383808b735ac170181fef210729ac0895afe44
fritzschoff <fritzschoff@icloud.com>
2024-05-29 14:28:37 +0100

    feat(useAPR): added support if api returns array

--- liquidity/lib/useApr/useApr.ts
+++ liquidity/lib/useApr/useApr.ts
@@ -7,14 +7,20 @@ export function useApr() {
   return useQuery({
     queryKey: ['apr', network?.id],
     queryFn: async () => {
+      // 0 meaning not the right network
+      if (network?.id === 8453 || network?.id === 84532) return { combinedApr: 0 };
       try {
         const response = await fetch('https://api.synthetix.io/v3/base/sc-pool-apy');
 
         const data = await response.json();
-
+        if (Array.isArray(data)) {
+          return {
+            // 0 meaning not the right network
+            combinedApr: data[0].aprCombined * 100,
+          };
+        }
         return {
-          // 0 meaning not the right network
-          combinedApr: network?.id === 8453 || network?.id === 84532 ? data.aprCombined * 100 : 0,
+          data.aprCombined * 100
         };
       } catch (error) {
         return;
