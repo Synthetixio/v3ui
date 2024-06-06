b4b0ed10e4e4916799b0caf6f5a8388ca3cb8522
Noisekit <noisekit@proton.me>
2024-06-06 12:19:04 +1000

    Restructure and simplify Liquidity tests

--- liquidity/cypress/cypress/e2e/Manage Position - Andromeda USDC Deposit.e2e.js
+++ liquidity/cypress/cypress/e2e/Manage Position - Andromeda USDC Deposit.e2e.js
@@ -1,14 +1,14 @@
 import { generatePath } from 'react-router-dom';
 
-it('should deposit additional WETH collateral', () => {
+it('should deposit additional USDC collateral', () => {
   cy.task('isBase').then((isBase) => {
-    if (isBase) {
+    if (!isBase) {
       return;
     }
+
     cy.connectWallet().then(({ address, privateKey }) => {
       cy.task('setEthBalance', { address, balance: 100 });
-      cy.task('wrapEth', { privateKey: privateKey, amount: 50 });
-      cy.task('approveCollateral', { privateKey: privateKey, symbol: 'WETH' });
+      cy.task('getUSDC', { address: address, amount: 500 });
       cy.task('createAccount', { privateKey }).then((accountId) => {
         cy.wrap(accountId).as('accountId');
       });
@@ -16,7 +16,7 @@ it('should deposit additional WETH collateral', () => {
 
     cy.get('@accountId').then(async (accountId) => {
       const path = generatePath('/positions/:collateralSymbol/:poolId', {
-        collateralSymbol: 'WETH',
+        collateralSymbol: 'USDC',
         poolId: 1,
       });
       cy.visit(`/#${path}?manageAction=deposit&accountId=${accountId}`);
@@ -24,18 +24,17 @@ it('should deposit additional WETH collateral', () => {
     });
 
     cy.task('mineBlock');
+
     cy.get('[data-cy="manage-action-deposit"]').click();
-
-    cy.get('[data-testid="deposit amount input"]').type('10');
-
+    cy.get('[data-testid="deposit amount input"]').type('101');
     cy.get('[data-testid="deposit submit"]').should('be.enabled').click();
 
     cy.get('[data-cy="deposit-modal"]').should('exist').and('include.text', 'Complete this action');
 
     cy.get('[data-cy="deposit-modal"]')
-      .should('include.text', 'Approve WETH transfer')
-      .and('include.text', 'Delegate WETH')
-      .and('include.text', 'This will deposit and delegate 10 WETH to Spartan Council Pool.');
+      .should('include.text', 'Approve USDC transfer')
+      .and('include.text', 'Delegate USDC')
+      .and('include.text', 'This will deposit and delegate 101 USDC to Spartan Council Pool');
 
     cy.get('[data-cy="deposit-confirm-button"]').should('include.text', 'Start').click();
 
@@ -44,18 +43,17 @@ it('should deposit additional WETH collateral', () => {
       .and('be.enabled')
       .click();
 
-    cy.get('[data-cy="manage stats collateral"]').should('include.text', '10 WETH');
-
-    cy.get('[data-testid="deposit amount input"]').type('10');
+    cy.get('[data-cy="manage stats collateral"]').should('include.text', '101 USDC');
+    cy.get('[data-testid="deposit amount input"]').type('101');
 
     cy.get('[data-testid="deposit submit"]').should('be.enabled').click();
 
     cy.get('[data-cy="deposit-modal"]').should('exist').and('include.text', 'Complete this action');
 
     cy.get('[data-cy="deposit-modal"]')
-      .should('include.text', 'Approve WETH transfer')
-      .and('include.text', 'Delegate WETH')
-      .and('include.text', 'This will deposit and delegate 10 WETH to Spartan Council Pool.');
+      .should('include.text', 'Approve USDC transfer')
+      .and('include.text', 'Delegate USDC')
+      .and('include.text', 'This will deposit and delegate 101 USDC to Spartan Council Pool');
 
     cy.get('[data-cy="deposit-confirm-button"]').should('include.text', 'Start').click();
 
@@ -66,6 +64,6 @@ it('should deposit additional WETH collateral', () => {
 
     cy.get('[data-cy="deposit-modal"]').should('not.exist');
 
-    cy.get('[data-cy="manage stats collateral"]').should('include.text', '20 WETH');
+    cy.get('[data-cy="manage stats collateral"]').should('include.text', '202 USDC');
   });
 });
