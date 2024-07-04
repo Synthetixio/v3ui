b5ad1854f537e38ff76d157af0201cc8adc0d157
MrX-SNX <max@cc.snxdao.io>
2024-07-04 14:09:42 +0100

    ref: deps in useEffect hook

--- governance/ui/src/hooks/useCountdown.tsx
+++ governance/ui/src/hooks/useCountdown.tsx
@@ -1,34 +1,34 @@
 import { useState, useEffect } from 'react';
 
+const calculateTimeLeft = (timestamp: number) => {
+  const now = new Date();
+  const targetDate = new Date(timestamp * 1000);
+  const difference = targetDate.getTime() - now.getTime();
+
+  let timeLeft = {} as { days: number; minutes: number; hours: number };
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+    };
+  } else {
+    timeLeft = { days: 0, hours: 0, minutes: 0 };
+  }
+
+  return timeLeft;
+};
+
 const useCountdown = (timestamp: number) => {
-  const calculateTimeLeft = () => {
-    const now = new Date();
-    const targetDate = new Date(timestamp * 1000); // Convert timestamp from seconds to milliseconds
-    const difference = targetDate.getTime() - now.getTime();
-
-    let timeLeft = {} as { days: number; minutes: number; hours: number };
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-      };
-    } else {
-      timeLeft = { days: 0, hours: 0, minutes: 0 };
-    }
-
-    return timeLeft;
-  };
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(timestamp));
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 60000); // Update every minute
+      setTimeLeft(calculateTimeLeft(timestamp));
+    }, 60000);
 
-    return () => clearInterval(timer); // Cleanup the interval on component unmount
+    return () => clearInterval(timer);
   }, [timestamp]);
 
   return timeLeft;
