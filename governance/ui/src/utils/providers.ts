ef3f619928824829e750552e7e667551a2b92195
MrX-SNX <max@cc.snxdao.io>
2024-07-16 17:13:36 +0100

    test: added cypress config

--- governance/ui/src/utils/providers.ts
+++ governance/ui/src/utils/providers.ts
@@ -13,21 +13,3 @@ export const devSigner = new Wallet(
   '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
   motherShipProvider
 );
-
-// // Mumbai for testnet
-// export const satellite1Provider = new providers.JsonRpcProvider(
-//   process.env.DEV
-//     ? 'https://endpoints.omniatech.io/v1/matic/mumbai/public'
-//     : process.env.TESTNET
-//       ? `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY}`
-//       : 'https://mainnet.optimism.io'
-// );
-//
-// // OP Goerli for testnet
-// export const satellite2Provider = new providers.JsonRpcProvider(
-//   process.env.DEV
-//     ? 'https://endpoints.omniatech.io/v1/op/goerli/public'
-//     : process.env.TESTNET
-//       ? `https://optimistic-goerli.infura.io/v3/${process.env.INFURA_KEY}`
-//       : 'https://mainnet.optimism.io'
-// );
