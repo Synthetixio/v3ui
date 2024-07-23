561a31b9db44aa0c42ee09b184db1532faf3a8af
jmzwar <james.zwar@protonmail.com>
2024-07-23 09:03:00 +0200

    fix for positions apr

--- liquidity/ui/src/components/Positions/PositionsList.tsx
+++ liquidity/ui/src/components/Positions/PositionsList.tsx
@@ -32,7 +32,7 @@ export const PositionsList = () => {
       <PositionsTable
         isLoading={isLoading}
         positions={parsedPositions}
-        apr={apr?.combinedApr}
+        apr={apr?.collateralAprs}
         systemToken={systemToken}
       />
     </Flex>
--- liquidity/ui/src/components/Positions/PositionsTable/PositionsTable.tsx
+++ liquidity/ui/src/components/Positions/PositionsTable/PositionsTable.tsx
@@ -15,7 +15,7 @@ import { isBaseAndromeda } from '@snx-v3/isBaseAndromeda';
 interface PositionsTableInterface {
   isLoading: boolean;
   positions?: LiquidityPositionType[];
-  apr?: number;
+  apr?: any[];
   systemToken?: {
     symbol?: string;
     name?: string;
@@ -96,16 +96,24 @@ export const PositionsTable = ({
                 <PositionsRowLoading />
               ) : (
                 <>
-                  {positions?.map((position, index) => (
-                    <PositionRow
-                      key={position.poolName.concat(index.toString())}
-                      {...position}
-                      final={index === positions.length - 1}
-                      isBase={isBase}
-                      apr={apr}
-                      systemTokenSymbol={systemToken?.symbol}
-                    />
-                  ))}
+                  {positions?.map((position, index) => {
+                    const positionApr = apr?.find(
+                      (apr) =>
+                        apr.collateralType.toLowerCase() ===
+                        position.collateralType.tokenAddress.toLowerCase()
+                    );
+
+                    return (
+                      <PositionRow
+                        key={position.poolName.concat(index.toString())}
+                        {...position}
+                        final={index === positions.length - 1}
+                        isBase={isBase}
+                        apr={positionApr?.apr28d * 100}
+                        systemTokenSymbol={systemToken?.symbol}
+                      />
+                    );
+                  })}
                 </>
               )}
             </Tbody>
