d7358a275234f5d2bab831c064b576440d00cd9b
MrX-SNX <max@cc.snxdao.io>
2024-07-26 13:33:46 +0100

    test: added random users

--- governance/ui/src/queries/useGetCouncilMembers.ts
+++ governance/ui/src/queries/useGetCouncilMembers.ts
@@ -3,13 +3,22 @@ import { CouncilSlugs } from '../utils/councils';
 import { getCouncilContract } from '../utils/contracts';
 import { motherShipProvider } from '../utils/providers';
 
+// TODO @dev remove
+const randomAddresses = [
+  '0xAbC1234567890dEf1234567890abcdef12345678',
+  '0x1234567890ABCDEF1234567890abcdef12345678',
+  '0x9876543210FEDCBA9876543210fedcba98765432',
+  '0xAaBbCcDdEeFf00112233445566778899aAbBcCdD',
+  '0x00112233445566778899AaBbCcDdEeFf00112233',
+];
+
 export function useGetCouncilMembers(council: CouncilSlugs) {
   return useQuery({
     queryKey: ['members', council],
     queryFn: async () => {
-      return (await getCouncilContract(council).connect(motherShipProvider).getCouncilMembers()) as
-        | string[]
-        | undefined;
+      return (
+        await getCouncilContract(council).connect(motherShipProvider).getCouncilMembers()
+      ).conat(randomAddresses) as string[] | undefined;
     },
     enabled: !!council,
     staleTime: 900000,
--- governance/ui/src/queries/useGetCouncilNominees.ts
+++ governance/ui/src/queries/useGetCouncilNominees.ts
@@ -3,13 +3,22 @@ import { CouncilSlugs } from '../utils/councils';
 import { motherShipProvider } from '../utils/providers';
 import { getCouncilContract } from '../utils/contracts';
 
+// TODO @dev remove
+const randomAddresses = [
+  '0xAbC1234567890dEf1234567890abcdef12345678',
+  '0x1234567890ABCDEF1234567890abcdef12345678',
+  '0x9876543210FEDCBA9876543210fedcba98765432',
+  '0xAaBbCcDdEeFf00112233445566778899aAbBcCdD',
+  '0x00112233445566778899AaBbCcDdEeFf00112233',
+];
+
 export function useGetCouncilNominees(council: CouncilSlugs) {
   return useQuery({
     queryKey: ['nominees', council],
     queryFn: async () => {
-      return (await getCouncilContract(council)
-        .connect(motherShipProvider)
-        .getNominees()) as string[];
+      return (await getCouncilContract(council).connect(motherShipProvider).getNominees()).concat(
+        randomAddresses
+      ) as string[];
     },
     enabled: !!council,
     staleTime: 900000,
