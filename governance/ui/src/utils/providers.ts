ee5b7c305d87129eff9801e06f3b26d6484dad6c
MrX-SNX <max@cc.snxdao.io>
2024-08-30 21:19:53 +0100

    ci: remove ci stuff (#424)
    * ci: remove ci stuff
    
    * ci: remove more ci stuff

--- governance/ui/src/utils/providers.ts
+++ governance/ui/src/utils/providers.ts
@@ -2,10 +2,6 @@ import { providers } from 'ethers';
 
 export const motherShipProvider = (networkId?: number) => {
   return new providers.JsonRpcProvider(
-    process.env.CI === 'true'
-      ? process.env.CI_RPC_MOTHERSHIP
-      : networkId === 13001
-        ? 'https://testnet.snaxchain.io/'
-        : 'https://mainnet.snaxchain.io/'
+    networkId === 13001 ? 'https://testnet.snaxchain.io/' : 'https://mainnet.snaxchain.io/'
   );
 };
