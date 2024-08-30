d97eb6fb1c8cadf1d7b2e8c174c30ac7f100a0f0
MrX-SNX <max@cc.snxdao.io>
2024-08-30 21:00:52 +0100

    ci: remove ci stuff

--- governance/ui/src/utils/contracts.ts
+++ governance/ui/src/utils/contracts.ts
@@ -28,16 +28,10 @@ const TreasuryCouncilContract = new Contract(
 export function getCouncilContract(council: CouncilSlugs) {
   switch (council) {
     case 'spartan':
-      if (process.env.CI === 'true')
-        return SpartanCouncilContract.connect('0xBC85F11300A8EF619592fD678418Ec4eF26FBdFD');
       return SpartanCouncilContract;
     case 'ambassador':
-      if (process.env.CI === 'true')
-        return AmbassadorCouncilContract.connect('0xCdbEf5753cE3CEbF361e143117e345ADd7498F80');
       return AmbassadorCouncilContract;
     case 'treasury':
-      if (process.env.CI === 'true')
-        return TreasuryCouncilContract.connect('0xe3aB2C6F1C9E46Fb53eD6b297c6fff68e935B161');
       return TreasuryCouncilContract;
     default:
       throw new Error('could not find contract');
@@ -46,16 +40,6 @@ export function getCouncilContract(council: CouncilSlugs) {
 
 export const SnapshotRecordContract = (chainId: number, council: CouncilSlugs) => {
   switch (chainId) {
-    case 13001: {
-      switch (council) {
-        case 'spartan':
-          return new Contract('0x552E469B7C88cd501C08e7759d35dC58f08C9648', abiForSnapshotMock);
-        case 'ambassador':
-          return new Contract('0x3a0186E03137B9b971EC911350A0F2D88D24FDF2', abiForSnapshotMock);
-        case 'treasury':
-          return new Contract('0xC0bFA9aC792cF691734F7b2BD252d1c2B9fBa343', abiForSnapshotMock);
-      }
-    }
     case 10: {
       switch (council) {
         case 'spartan':
