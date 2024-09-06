9755afa23a73878f69dd01204b443c41a7297b9d
MrX-SNX <max@cc.snxdao.io>
2024-09-06 23:36:25 +0100

    feat: safe tx (#445)
    * feat: safe tx
    
    * ref: calc for every tx
    
    * ref: typecheck

--- governance/ui/src/mutations/useCastVotes.ts
+++ governance/ui/src/mutations/useCastVotes.ts
@@ -124,10 +124,19 @@ export function useCastVotes(
                 };
           });
 
+          const isSafe = (await signer.provider.getCode(activeWallet?.address || '')).length > 3;
+          const gas = (
+            await Promise.all(
+              prepareBallotData
+                .map(async (tx) => await signer.provider.estimateGas({ ...tx }))
+                .concat(castData.map(async (tx) => await signer.provider.estimateGas({ ...tx })))
+            )
+          ).reduce((cur, next) => cur.add(next), BigNumber.from(0));
           await multicall
             .connect(signer)
             [isMC ? 'aggregate' : 'aggregate3Value']([...prepareBallotData, ...castData], {
               value: isMC ? 0 : quote.add(quote.mul(25).div(100)).mul(councils.length),
+              gasLimit: isSafe ? gas : undefined,
             });
         } catch (error: any) {
           console.error(error);
